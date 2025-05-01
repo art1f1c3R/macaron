199f7f97a21259b8d16e4d40f9a8abbf63c6b1e7
Carl Flottmann <carl.flottmann@oracle.com>
2025-05-01 15:24:45 +1000

    build: updated dockerfile, docker build commands, and existing github actions
    Signed-off-by: Carl Flottmann <carl.flottmann@oracle.com>

--- scripts/dev_scripts/build_docker.sh
+++ scripts/dev_scripts/build_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright (c) 2023 - 2023, Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2023 - 2025, Oracle and/or its affiliates. All rights reserved.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/.
 
 # This script is used to build the Docker image. The built image will always be tagged with `latest`.
@@ -24,8 +24,16 @@ then
     exit 1
 fi
 
+SEMGREP_WHEEL_PATH=$(find "$DIST_PATH" -depth -type f -name 'semgrep-*.whl' | head -n 1)
+if [[ -z "${SEMGREP_WHEEL_PATH}" ]];
+then
+    echo "Unable to find Semgrep wheel file in ${DIST_PATH}."
+    exit 1
+fi
+
 # We need to use the relative path so that it works in the docker context.
 WHEEL_PATH=$(realpath --relative-to="$REPO_PATH" "$WHEEL_PATH")
+SEMGREP_WHEEL_PATH=$(realpath --relative-to="$REPO_PATH" "$SEMGREP_WHEEL_PATH")
 
 if [[ -z "${RELEASE_TAG}" ]];
 then
@@ -33,12 +41,14 @@ then
         -t "${IMAGE_NAME}:latest" \
         -t "${IMAGE_NAME}:test" \
         --build-arg WHEEL_PATH="${WHEEL_PATH}" \
+        --build-arg SEMGREP_WHEEL_PATH="${SEMGREP_WHEEL_PATH}" \
         -f "${REPO_PATH}/docker/Dockerfile.final" "${REPO_PATH}"
 else
     docker build \
         -t "${IMAGE_NAME}:latest" \
         -t "${IMAGE_NAME}:${RELEASE_TAG}" \
         --build-arg WHEEL_PATH="${WHEEL_PATH}" \
+        --build-arg SEMGREP_WHEEL_PATH="${SEMGREP_WHEEL_PATH}" \
         -f "${REPO_PATH}/docker/Dockerfile.final" "$REPO_PATH"
 fi
 
